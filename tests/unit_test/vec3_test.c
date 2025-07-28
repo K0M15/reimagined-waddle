23fd4cde5c8930cd3a5f641181c40d7c254cef02
Kristaps Zarins <kzarins@2-F-7.42heilbronn.de>
2025-07-28 15:30:00 +0200

    Add test that still has compilation errors.

---
+++ tests/unit_test/vec3_test.c
@@ -0,0 +1,20 @@
+#include "settings.h"
+#include "vec3.h"
+#include <stdlib.h>
+
+int vec3_test(void)
+{
+	vec3 a{.x=1, .y=1, .z=2}, b{.x=2, .y=3, .z=4};
+	FLOAT dot_prod = vec3_dot_product(a,b);
+	vec3 *cross_prod = vec3_cross_procuct_r(a,b);
+	printf("The vec3 a:\n");
+	vec3_print(a);
+	printf("The vec3 b:\n");
+	vec3_print(b);
+	printf("The dot product is: %f\n", dot_prod);
+	printf("The cross product is: ");
+	vec3_print(cross_prod);
+
+	free(cross_prod);
+	return (1);
+}
