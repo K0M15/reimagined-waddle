e52bcf21e3fe04d96fa5d962cb5491c263c7f0aa
Kristaps Zarins <kzarins@2-H-3.42heilbronn.de>
2025-07-30 15:27:35 +0200

    Add the structures for the default objects

--- include/settings.h
+++ include/settings.h
@@ -1,5 +1,10 @@
 #ifndef SETTINGS_H
 # define SETTINGS_H
 
+//Configures all calculations to FLOAT
 #define FLOAT float
+
+//Makes intersectin precision of 1/256 deg.
+#define EPSILON_INTERSECT 0.000068
+
 #endif
