507a64bf66a31a983cc23fa4a4b278bc0c145e00
Kristaps Zarins <kzarins@2-F-2.42heilbronn.de>
2025-10-13 14:10:59 +0200

    Make changes to minirt.h header.

--- include/minirt.h
+++ include/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   miniRT.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afelger <afelger@student.42.fr>            +#+  +:+       +#+        */
+/*   By: kzarins <kzarins@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 10:14:42 by afelger           #+#    #+#             */
-/*   Updated: 2025/10/10 09:29:52 by afelger          ###   ########.fr       */
+/*   Updated: 2025/10/10 13:18:53 by kzarins          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,8 @@ typedef struct s_app
     t_dyn   hitable;
 }	t_app;
 
+int32_t pars_init(int argc, char **argv);
+
 t_vec3 ftray_color(t_ray ray, t_dyn *arr, int depth);
 uint32_t ft_camera_init(t_camera *camera, t_camera_p props);
 void ft_camera_calc(t_camera *camera);
@@ -81,4 +83,46 @@ void ft_camera_apply(t_camera *cam, t_vec3 apply);
 uint32_t ft_camera_render(
     t_app *app,
     void (*put_pixel)(mlx_image_t *image, int x, int y, uint32_t color));
-#endif /* MINI_RT_H */
+
+//TODO: Make it cleaner
+
+#include "elements.h"
+#include "ft_ll.h"
+
+//typedef struct s_scene {
+//	t_ambient_light	*ambient_light;
+//	t_camera 	*camera;
+//	t_light		*light;
+//	t_sphere	*sphere;
+//	t_plane		*plane;
+//	t_cylinder	*cylinder;
+//} t_scene;
+
+//The global settings for the viewport
+typedef struct	s_settings {
+	FLOAT	viewport_height;
+	FLOAT	viewport_width;
+	t_vec3	pixel_delta_h;
+	t_vec3	pixel_delta_w;
+	t_point	viewport_left;
+	t_point	first_pixel;
+} t_settings;
+
+//Because C compiles in row major order the order is pixel[y][x]
+//Pixels should be stored with values from 0 - 255
+typedef struct s_scene {
+	t_rgb	pixels[IMAGE_HEIGHT][IMAGE_WIDTH];
+	t_node	*ambient_light;
+	t_node 	*camera;
+	t_node	*light;
+	t_node	*sphere;
+	t_node	*plane;
+	t_node	*cylinder;
+	t_settings	settings;
+} t_scene;
+
+t_scene *get_scene(void);
+void	init_scene(void);
+int	render(const char * input);
+
+    #endif /* MINI_RT_H */
