a16002eb14c599268d1eb16b07fa9b198acec622
Alain Felger <alain.felger93+git@gmail.com>
2025-10-23 11:10:29 +0200

    added reference to uv_sphere

--- src/uv.c
+++ src/uv.c
@@ -6,7 +6,7 @@
 /*   By: afelger <alain.felger@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 10:51:08 by afelger           #+#    #+#             */
-/*   Updated: 2025/10/23 11:05:00 by afelger          ###   ########.fr       */
+/*   Updated: 2025/10/23 11:08:17 by afelger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 #include "hitable.h"
 #include "math.h"
 
+/*
+Reference
+https://gamedev.stackexchange.com/questions/114412/how-to-get-uv-coordinates-for-sphere-cylindrical-projection
+*/
 t_uv	uv_sphere(t_props sphere, t_vec3 p)
 {
 	t_vec3	normal;
@@ -22,7 +26,7 @@ t_uv	uv_sphere(t_props sphere, t_vec3 p)
 
 	normal = ftvec3_unit(ftvec3_divide(ftvec3_minus(p, sphere.position), ftvec3(sphere.radius)));
 	uu = 0.5f + atan2f(normal.z, normal.x) / (2.f * PI);
-	vv = 0.5f - asinf(clamp01(normal.y))   / PI;
+	vv = 0.5f - asinf(clamp(normal.y, 0.f, 1.0f))   / PI;
 	return ((t_uv){uu - floorf(uu), clamp(vv, 0.f, 1.0f)});
 }
 
