02a97a83a4c4e7ec26e0caf9ed572b0a9d157478
Alain Felger(afelger) <alain.felger93@gmail.com>
2025-11-04 15:46:53 +0100

    scalar script for triangle objects to make them fit the light config

---
+++ scale.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+from sys import argv
+
+def scale(filename:str, scale:float):
+    f = None
+    try:
+        f = open(filename, "r")
+    except OSError as e:
+        print("File could not be opened: ", e)
+        exit(1)
+    ctr = 0
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        parts = line.split(' ')
+        if (parts[0] == "tr"):
+            try:
+                vals = [part.split(',') for part in parts[1:4]]
+                conv = [[float(val)*scale for val in block] for block in vals]
+                print(f"tr {conv[0][0]},{conv[0][1]},{conv[0][2]} {conv[1][0]},{conv[1][1]},{conv[1][2]} {conv[1][0]},{conv[1][1]},{conv[1][2]} {parts[4]}", end="")
+            except:
+                print( f"Error on line {ctr} converting tr values" )
+                exit(1)
+        else:
+            print(line)
+    print()
+                
+    
+def usage():
+    print("Usage: ./scale.py <filename> <scalefactor>")
+    print("You may redirect the output to a file")
+    exit(1)
+
+if __name__ == "__main__":
+    if not len(argv) == 3:
+        usage()
+    newscale = None
+    try:
+        newscale = float(argv[2])
+    except ValueError:
+        print("Wrong value for scale, must be convertable to float")
+        usage()
+    scale(argv[1], newscale)
